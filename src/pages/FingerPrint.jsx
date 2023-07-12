5f4200ee65cb029b1aadfadf62f75fbbc2f20005
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2023-07-12 11:01:29 +0530

    updated scanning interfaces

--- src/pages/FingerPrint.jsx
+++ src/pages/FingerPrint.jsx
@@ -1,7 +1,3 @@
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Image from 'mui-image'
@@ -12,7 +8,6 @@ export default function FingerPrint() {
   return (
     <>
       <Container component="main" maxWidth="xs">
-        <CssBaseline />
         <Typography
           variant="h5"
           align="center"
