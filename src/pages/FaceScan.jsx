37919de2b045bc4084a9ba10743bd216d9595401
poornaSavindi <prnsavindi11@gmail.com>
2023-07-12 09:53:41 +0530

    Add face scan ui

---
+++ src/pages/FaceScan.jsx
@@ -0,0 +1,32 @@
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Image from "mui-image";
+import ProgressBar from "../components/ProgressBar";
+
+export default function FaceScan() {
+  return (
+    <>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Typography
+          variant="h5"
+          align="center"
+          gutterBottom
+          sx={{
+            fontWeight: "bold",
+            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.4)",
+          }}
+        >
+          Face Scan
+        </Typography>
+        <Image src="../src/assets/image/fingerprint.jpg" height="60vh"></Image>
+        <br />
+        <ProgressBar />
+      </Container>
+    </>
+  );
+}
--- src/routes/ViewRoutes.jsx
+++ src/routes/ViewRoutes.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import { HOME, ASKINFO, APP, LOGIN, FINGERPRINTLOAD } from '../constants/constants';
+import { HOME, ASKINFO, APP, LOGIN, FINGERPRINTLOAD, FACESCAN } from '../constants/constants';
 import Home from '../pages/Home';
 import InfoAsker from '../pages/UserInfoInput';
 import MainComponent from '../pages/Main';
 import LogIn from '../pages/Login';
 import FingerPrint from '../pages/FingerPrint';
+import FaceScan from '../pages/FaceScan'
 
 const ViewRoutes = () => {
   return (
@@ -15,6 +16,7 @@ const ViewRoutes = () => {
         <Route exact path={HOME} element={<Home />} />
         <Route exact path={ASKINFO} element={<InfoAsker />} />
         <Route exact path={FINGERPRINTLOAD} element={<FingerPrint/>}/> 
+        <Route exact path={FACESCAN} element={<FaceScan/>}/> 
       </Route>
     </Routes>
   );
